38a821b2e0ee65e49efe66da446f097adb8cc11c
Felix <pnguyen72@my.bcit.ca>
2026-03-31 21:41:20 -0700

    update demo

--- demo.d.ts
+++ demo.d.ts
@@ -1,4 +1,24 @@
 import type { calculate } from "./lib/calculator";
+import type { parse } from "./lib/json";
 
 type _calculator =
 	calculate<"-(-4(7-1) - 2*-3 + 1.9) + -2/3.2(2+-9) * --4(+11/2*+5)">;
+
+type _json = parse<`
+{
+	"name": "ts-parser",
+	"description": "Type-level parser combinator",
+	"license": "MIT",
+	"keywords": [
+		"parser",
+		"combinator",
+		"monadic",
+		"type-level"
+	],
+	"devDependencies": {
+		"@typescript/native-preview": "7.0.0-dev.20260330.1",
+		"ts-arithmetic": "^0.1.1"
+	}
+}
+
+`>;
